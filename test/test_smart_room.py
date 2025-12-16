d1201d35e4bb6f7dc66418af9fe9ff4e8003bd25
0verflow <staniswil@gmail.com>
2025-12-16 14:51:24 +0100

    User story #3 red phase

--- test/test_smart_room.py
+++ test/test_smart_room.py
@@ -38,3 +38,14 @@ class TestSmartRoom(unittest.TestCase):
         outcome = room.check_enough_light()
         self.assertFalse(outcome)
 
+
+    @patch.object(GPIO, "output")
+    @patch.object(SmartRoom, "check_enough_light")
+    @patch.object(SmartRoom, "check_room_occupancy")
+    def test_person_in_room_not_enough_light(self, mock_infrared_sensor: Mock, mock_photoresistor: Mock, mock_led: Mock):
+        mock_infrared_sensor.return_value = True
+        mock_photoresistor.return_value = False
+        room = SmartRoom()
+        room.manage_light_level()
+        mock_led.assert_called_once_with(room.LED_PIN, True)
+        self.assertTrue( room.light_on)
